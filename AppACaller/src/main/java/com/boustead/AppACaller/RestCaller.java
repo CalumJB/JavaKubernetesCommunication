8bf2269c05e674987c06f8377aa41d9246aa0534
calum boustead <calumboustead@calums-mbp.mynet>
2020-11-24 19:15:12 +0000

    App A communication with App B inside of the cluster by making GET request to cluster ip service

--- AppACaller/src/main/java/com/boustead/AppACaller/RestCaller.java
+++ AppACaller/src/main/java/com/boustead/AppACaller/RestCaller.java
@@ -4,6 +4,8 @@ import org.apache.commons.httpclient.*;
 import org.apache.commons.httpclient.methods.*;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,31 +15,34 @@ import java.io.IOException;
 @RestController
 public class RestCaller {
 
-    @GetMapping("/getDataFromA")
+    Logger logger = LoggerFactory.getLogger(RestCaller.class);
+
+    @Value("${app.b.ip}")
+    private String appBIP;
+
+    @Value("${app.b.port}")
+    private String appBPort;
+
+    @GetMapping("/getDataFromB")
     public String getDataFromA(){
 
-        @Value({"${ip}")
-        String ip;
+        logger.info("App B ip is: " + appBIP);
+        logger.info("App B port is: " + appBPort);
 
-        @Value({"${port}")
-        String ip;
+        System.out.println(appBIP + ":" + appBPort);
 
-        @Value({"${}")
-        String ip;
-
-        String ip = System.getProperty("ip");
-        String port = System.getProperty("port");
-
-        System.out.println(ip + ":" + port);
-
-        String url = "http://" + ip + ":" + port + "/returnData";
+        String url = "http://" + appBIP + ":" + appBPort + "/returnData";
 
         HttpClient client = new HttpClient();
+        logger.info("Created HttpClient");
         GetMethod method = new GetMethod(url);
+        logger.info("Created GetMethod");
+
         try {
+            logger.info("Entered try");
             // Execute the method.
             int statusCode = client.executeMethod(method);
-
+            logger.info("Status code is: " + statusCode);
             if (statusCode != HttpStatus.SC_OK) {
                 System.err.println("Method failed: " + method.getStatusLine());
             }
@@ -48,12 +53,14 @@ public class RestCaller {
             // Deal with the response.
             // Use caution: ensure correct character encoding and is not binary data
             System.out.println(new String(responseBody));
-            return new String(responseBody);
+            return "This is App A, App B said: " + new String(responseBody);
 
         } catch (HttpException e) {
+            logger.error("HttpException: " + e.getMessage());
             System.err.println("Fatal protocol violation: " + e.getMessage());
             e.printStackTrace();
         } catch (IOException e) {
+            logger.error("IOException: " + e.getMessage());
             System.err.println("Fatal transport error: " + e.getMessage());
             e.printStackTrace();
         } finally {
